b72ef373144e90298f4b1ff6eeda75791894e6a3
11062808 <43190565+sjjyy@users.noreply.github.com>
2019-09-28 01:12:20 +0900

    Attendance page

--- app/src/main/java/com/example/san/MainActivity.java
+++ app/src/main/java/com/example/san/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_id, tv_pass;
+
+    Button btn_attend;
 
 
     @Override
@@ -26,7 +30,14 @@ public class MainActivity extends AppCompatActivity {
         tv_id.setText(userID);
         tv_pass.setText(userPass);
 
-
+        btn_attend = findViewById (R.id.btn_attend);
+        btn_attend.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, AttendActivity.class);
+                startActivity (intent);
+            }
+        });
 
     }
 }
