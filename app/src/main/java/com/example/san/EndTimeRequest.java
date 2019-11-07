ad3fd3a1d71d721e649118156b845724e2bed985
ksooyeon <cr7070@hanmail.net>
2019-11-07 13:57:04 +0900

    출석 시간 체크

--- app/src/main/java/com/example/san/EndTimeRequest.java
+++ app/src/main/java/com/example/san/EndTimeRequest.java
@@ -12,14 +12,13 @@ public class EndTimeRequest extends StringRequest {
     final static private String URL = "http://san19.dothome.co.kr/EndTimeUpdate.php";
     private Map<String, String> map;
 
-    public EndTimeRequest(String userID, String courseID, String endTime, String attdState, String etc, Response.Listener<String> listener) {
+    public EndTimeRequest(String userID, String courseID, String endTime, String etc, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
         map.put("userID", userID);
         map.put("courseID", courseID);
         map.put("endTime", endTime);
-        map.put("attdState", attdState);
         map.put("etc", etc);
     }
 
